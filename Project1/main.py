f6aaa44eb5ffc3a246fdd9150a3921375070a3c0
brayanmontiel1 <67293845+brayanmontiel1@users.noreply.github.com>
2021-11-04 21:03:22 -0700

    added new method print and goalchecker

--- Project1/main.py
+++ Project1/main.py
@@ -8,38 +8,45 @@ def main():
     print('Project 1: CS170 8 Puzzle - Brayan Montiel\n')
     print('Please choose an option below: ')
     # cin which option user wants to choose.
-    userOpt = input('1. Enter a custom 3x3 puzzle. \n' 
+    userOpt = input('1. Enter a custom 3x3 puzzle. \n'
                     '2. Choose puzzle for me. \n'
                     'Enter choice: ')
     userOpt = int(userOpt)
-
-    
-    if userOpt == 1:           #custom puzzle
-        print('Lets create your puzzle. Enter one 0 for missing piece. (Program assumes no incorrect input)')
-        temp1 = input('Enter the first row: ')
-        temp1 = temp1.split(' ')
-        temp2 = input('Enter the second row: ')
-        temp2 = temp2.split(' ')
-        temp3 = input('Enter the third row: ')
-        temp3 = temp3.split(' ')
-        print('\n')
-        arr = temp1, temp2, temp3 #append rows to make puzzle 3x3
-        print(arr)            
-    elif userOpt == 1:           #preset puzzle
-        arr = (['1', '0', '3'], 
-               ['4', '2', '6'], 
+    arr = []
+    if userOpt == 1:  # custom puzzle
+        print('Lets create your puzzle. Enter one 0 for missing piece. (Program assumes no incorrect input)\n')
+        #size = int(input()) 
+        print('Enter the 3x3 puzzle:')
+        for x in range(3):
+            arr.append([int(y) for y in input().split()])
+        print('/nYour puzzle:')
+        printPuzzle(arr)
+    elif userOpt == 2:  # preset puzzle
+        arr = (['1', '0', '3'],
+               ['4', '2', '6'],
                ['7', '5', '8'])
-        print(arr)
-
+        printPuzzle(arr)
     #choose which algorithm
-    userOpt = input('\nChoose an algorithm to solve puzzle: \n' 
-                 '1) Uniform Cost Search \n'
-                 '2) A* with the Misplaced Tile heuristic. \n' 
-                 '3) A* with the Manhattan Distance heuristic. \n'
+    userOpt = input('\nChoose an algorithm to solve puzzle: \n'
+                    '1) Uniform Cost Search \n'
+                    '2) A* with the Misplaced Tile heuristic. \n'
+                    '3) A* with the Manhattan Distance heuristic. \n'
                     'Enter choice: ')
     userOpt = int(userOpt)
-    #
-    #print(generalsearch(arr, userOpt))
+    #print(algo(arr, userOpt))
+    #if goalCheck(arr) == True: //used to see if goalCheck works
+
+#formats the array nicely#
+def printPuzzle(arr = []): 
+    print('\n'.join([''.join(['{:3}'.format(item) for item in row]) 
+      for row in arr]))
+
+#checks if the array is at goal state#
+def goalCheck(arr):
+    if goal_state == arr:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     main()
