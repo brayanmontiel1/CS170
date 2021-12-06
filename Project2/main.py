f31d16dc712445d75fe09e6617c322cf372a203f
brayanmontiel1 <67293845+brayanmontiel1@users.noreply.github.com>
2021-12-05 22:01:09 -0800

    updating menu

--- Project2/main.py
+++ Project2/main.py
@@ -4,23 +4,24 @@ import csv
 
 def menu():
     print('Project 2: CS170 Feature Classification - Brayan Montiel\n')
+
     #test data - change when testing assigned small-19 and large-13: 
     testSmall = 'CS170/Project2/testdata/Ver_2_CS170_Fall_2021_Small_data__86.txt'
     testLarge = 'CS170/Project2/testdata/Ver_2_CS170_Fall_2021_LARGE_data__27.txt'
 
-    #choose which algorithm - assuming no incorrect user input
+    #choose which dataset to run - assuming no incorrect user input
     filePick = input('\nPlease select a dataset to run: \n'
                      '1) Small dataset: Ver_2_CS170_Fall_2021_Small_data__86.txt \n'
                      '2) Large dataset: Ver_2_CS170_Fall_2021_LARGE_data__27.txt \n'
                      'Enter choice: ')
     filePick = int(filePick)
 
-    if(filePick == 1):
+    if(filePick == 1):      #small dataset
         try:
             data = open(testSmall, 'r')
         except:
             print('File ' + testLarge + ' not found.')
-    else:
+    else:                   #large dataset
         try:
             data = open(testLarge, 'r')
         except:
