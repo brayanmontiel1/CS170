fe7b69f323a50ce0150055d3f4782932aee557f4
brayanmontiel1 <67293845+brayanmontiel1@users.noreply.github.com>
2021-12-08 14:03:18 -0800

    adding backward elimination, wokring program. Have to test assigned datasets and send to prof.

--- Project2/main.py
+++ Project2/main.py
@@ -1,6 +1,4 @@
-import copy
 import time
-import os
 import numpy as panda
 from operator import itemgetter
 from collections import Counter
@@ -9,8 +7,8 @@ def menu():
     print('Project 2: CS170 Feature Classification - Brayan Montiel\n')
 
     #test data - change when testing assigned small-19 and large-13: 
-    testSmall = 'CS170/Project2/testdata/Ver_2_CS170_Fall_2021_Small_data__86.txt'
-    testLarge = 'CS170/Project2/testdata/Ver_2_CS170_Fall_2021_LARGE_data__27.txt'
+    testSmall = 'CS170/Project2/Ver_2_CS170_Fall_2021_Small_data__19.txt'
+    testLarge = 'CS170/Project2/Ver_2_CS170_Fall_2021_LARGE_data__13.txt'
 
     #choose which dataset to run - assuming no incorrect user input
     filePick = input('\nPlease select a dataset to run: \n'
@@ -24,7 +22,7 @@ def menu():
             data = panda.genfromtxt(testSmall)
             print('You picked: ' + testSmall + ' \n')
         except:
-            print('File ' + testLarge + ' not found.')
+            print('File ' + testSmall + ' not found.')
 
     elif(filePick == 2):     #large dataset
         try:
@@ -33,7 +31,7 @@ def menu():
         except:
             print('File ' + testLarge + ' not found.')
 
-    elif(filePick == 3):     #type in file path
+    elif(filePick == 3):    #type in file path
         testCustom = input('Enter file path: ')   #custom file path
         try:
             data = panda.genfromtxt(testCustom)
@@ -53,7 +51,7 @@ def menu():
     if(algoPick == 1):           #run forward selection
         print('Okay lets run Forward Selection')        
         accuracy = leave_one_out_x_valid(data)
-        print('Running nearest neighbor with all ', dataFeatures ,' features, using \"leaving-one-out\" evalutation, I get an accuracy of ',round(accuracy,2),'%.\n')
+        print('Running nearest neighbor with all ', dataFeatures ,' features, using \"leaving-one-out\" evaluation, I get an accuracy of ',round(accuracy,2),'%.\n')
         print('Beginning search.')
         start = time.time()
         forwardSelection(data)
@@ -63,9 +61,9 @@ def menu():
     elif(algoPick == 2):        #run backward elimination
         print('Okay lets run Backward Elimination')       
         accuracy = leave_one_out_x_valid(data)
-        print('Running nearest neighbor with all ', dataFeatures ,' features, using \"leaving-one-out\" evalutation, I get an accuracy of ',round(accuracy,2),'%.\n')
+        print('Running nearest neighbor with all ', dataFeatures ,' features, using \"leaving-one-out\" evaluation, I get an accuracy of ',round(accuracy,2),'%.\n')
         start = time.time()
-        #backwardElimination(data)
+        backwardElimination(data)
         finish = time.time()
         print('Total run Backward Elimination runtime: ', (finish-start))
 
@@ -143,8 +141,8 @@ def forwardSelection(data):
             print('\tUsing feature(s) ',current_set+[j] ,' accuracy is ', round(tempAccuracy,2), '%.' )
 
             if(tempMax < tempAccuracy):
-                tempMax = tempAccuracy      #assign max to current max accuracy 
                 featList = j
+                tempMax = tempAccuracy      #assign max to current max accuracy 
 
         if featList:    #if not 0
             current_set.append(featList)
@@ -157,5 +155,35 @@ def forwardSelection(data):
     print('\nFinished search! The best feature subset is ', featBest,', which has an accuracy of ', round(accuracyMax,2), '%.')
 
 
+def backwardElimination(data):          #backwards logic of forward selection
+    dataFeatures = data.shape[1]        #get num of features
+    featBest = list(range(1,dataFeatures))      #arr with best features   
+    current_set = list(range(1,dataFeatures))   #current set alr contains dataFeatures
+    accuracyMax = 0                     #maximum acc to return 
+
+    for i in range(1,dataFeatures):
+        tempMax = 0
+        featList = []
+        for j in range(1, dataFeatures):
+            if j not in current_set:            #skip if j is still not in the current set
+                continue
+            tempFeatures = [i for i in current_set if i != j]
+            tempAccuracy = leave_one_out_x_valid(data[:,[0]+ tempFeatures])
+            print('\tUsing feature(s) ', tempFeatures ,' accuracy is ', round(tempAccuracy,2), '%.' )
+
+            if(tempMax < tempAccuracy):
+                featList = j
+                tempMax = tempAccuracy      #assign max to current max accuracy 
+
+        if featList:    #if not 0
+            current_set = [k for k in current_set if k != featList]
+            if(accuracyMax < tempMax):  #check max btw tempMax and overall accuracy 
+                accuracyMax = tempMax
+                featBest[:] = current_set
+                print('Feature set ', current_set,' was best, accuracy is ', round(tempMax,2), '%.' )
+            else:
+                print('Feature set ', current_set,' was best, accuracy is ', round(tempMax,2), '%.' )
+    print('\nFinished search! The best feature subset is ', featBest,', which has an accuracy of ', round(accuracyMax,2), '%.')
+    
 if __name__ == "__main__":
     menu()
