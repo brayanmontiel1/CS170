84e3e48655e354bab01c8f0e579534a2613e3989
brayanmontiel1 <67293845+brayanmontiel1@users.noreply.github.com>
2021-12-08 01:38:41 -0800

    bpushing forward selection, working on test files so far

--- Project2/main.py
+++ Project2/main.py
@@ -14,8 +14,9 @@ def menu():
 
     #choose which dataset to run - assuming no incorrect user input
     filePick = input('\nPlease select a dataset to run: \n'
-                     '1) Small dataset: Ver_2_CS170_Fall_2021_Small_data__86.txt \n'
-                     '2) Large dataset: Ver_2_CS170_Fall_2021_LARGE_data__27.txt \n'
+                     '1) Small dataset: Ver_2_CS170_Fall_2021_Small_data__19.txt \n'
+                     '2) Large dataset: Ver_2_CS170_Fall_2021_LARGE_data__13.txt \n'
+                     '3) Type my own text file path \n'
                      'Enter choice: ')
     filePick = int(filePick)
     if(filePick == 1):      #small dataset
@@ -24,12 +25,21 @@ def menu():
             print('You picked: ' + testSmall + ' \n')
         except:
             print('File ' + testLarge + ' not found.')
-    else:                   #large dataset
+
+    elif(filePick == 2):     #large dataset
         try:
             data = panda.genfromtxt(testLarge)
             print('You picked: ' + testLarge + ' \n')
         except:
             print('File ' + testLarge + ' not found.')
+
+    elif(filePick == 3):     #type in file path
+        testCustom = input('Enter file path: ')   #custom file path
+        try:
+            data = panda.genfromtxt(testCustom)
+            print('File ' + testCustom + ' retrieved successfully.\n')
+        except:
+            print('File ' + testCustom + ' not found.')
 
     dataFeatures = data.shape[1]-1
     dataInstances = data.shape[0]
@@ -39,28 +49,30 @@ def menu():
                         '1) Forward Selection\n' +
                         '2) Backward Elimination\n')    #assumes no wrong input from user
     algoPick = int(algoPick)
-    if(algoPick == 1):                              #run forward selection
+
+    if(algoPick == 1):           #run forward selection
         print('Okay lets run Forward Selection')        
         accuracy = leave_one_out_x_valid(data)
-        print('Running nearest neighbor with all ', dataFeatures ,' features, using \"leaving-one-out\" evalutation, I get an accuracy of ',accuracy,'.\n')
+        print('Running nearest neighbor with all ', dataFeatures ,' features, using \"leaving-one-out\" evalutation, I get an accuracy of ',round(accuracy,2),'%.\n')
+        print('Beginning search.')
         start = time.time()
         forwardSelection(data)
         finish = time.time()
-        print('Total run Forward Search runtime: ', (finish-start))
+        print('Total run Forward Selection runtime: ', (finish-start))
 
-
-    elif(algoPick == 2):                            #run backward selection
-        print('Okay lets run Backward Selection')       
+    elif(algoPick == 2):        #run backward elimination
+        print('Okay lets run Backward Elimination')       
         accuracy = leave_one_out_x_valid(data)
-        print('Running nearest neighbor with all ', dataFeatures ,' features, using \"leaving-one-out\" evalutation, I get an accuracy of ',accuracy,'.\n')
+        print('Running nearest neighbor with all ', dataFeatures ,' features, using \"leaving-one-out\" evalutation, I get an accuracy of ',round(accuracy,2),'%.\n')
         start = time.time()
         #backwardElimination(data)
         finish = time.time()
-        print('Total run Forward Search runtime: ', (finish-start))
+        print('Total run Backward Elimination runtime: ', (finish-start))
 
 
 def leave_one_out_x_valid(data):            #performs leave out out validation, gets accuracy
     val = []        #values from nearest neighbors
+    classType = [i[0] for i in data]    #puts classifier in array 
     for i in range(len(data)):          #loop through data to clean exp
         tempData = []
         data_arr = data[i]              
@@ -72,43 +84,48 @@ def leave_one_out_x_valid(data):            #performs leave out out validation, 
             tempData =  data[:i]
         else:
             tempData = panda.concatenate((data[:i],data[i+1:]), axis=0)
+
         classTemp = [i[0] for i in tempData]
         tempData = panda.delete(tempData, 0, axis=1) # 0 out column 
         results = nearestNeighbor(data_arr, tempData, classTemp) ##data, current set, feature to add
         val.append(results)             #put results in val[]
-    classType = [i[0] for i in data]    #puts classifier in array 
+
     accResutls = accuracy(val, classType)   #get accuracy of results
     return accResutls
 
 
-def accuracy(values, classType):        #calculates accuracy of leave one out
+def accuracy(val, classType):        #calculates accuracy of leave one out
     count = 0
-    for i,j in zip(values, classType):
+    for i,j in zip(val, classType):
         if(i == j):
             count +=1
     result = (count/len(classType)) * 100
     return result
 
-
-def nearestNeighbor(data, current_set, classifications):    #returns nearest neighbors list
+def nearestNeighbor(data, current_set, classType):    #returns nearest neighbors list
     nearest = []    #nearest neighbor arr
-    distance = []   #distance arr
-    count = 0
-    total = 0
-    for i in current_set:               #loop through current_set
-        for j,k in zip(i,data):         #loop , zip can make it easier to go through and find k distance 
-            countDist = abs(j-k)**2     #calculate distance
-            total += countDist
-        total**(1/2)                          
-        distance.append((total,classifications[count]))
-        count +=1  
-    distance = sorted(distance, key=itemgetter(0))
-    k_dist = [a[1] for a in distance[:1]]           #get first column 
-
-    k_dist = list(Counter(k_dist).keys())
-    nearest.append(k_dist[0])
+    k_dist = nearestNeighborHelper(data,current_set,classType)
+    counter = list(Counter(k_dist).keys())
+    nearest.append(counter[0])
     return nearest
 
+def nearestNeighborHelper(data,current_set,classType):
+    distance = []   #distance arr
+    count = 0
+    for i in current_set:               #loop through current_set
+        totalDist = dist(i,data)              
+        distance.append((totalDist,classType[count]))
+        count +=1  
+    distance = sorted(distance, key=itemgetter(0))
+    k_dist = [l[1] for l in distance[:1]]           #k distance for every element in distance arr :1
+    return k_dist
+
+def dist(i,data):
+    total = 0
+    for j,k in zip(i,data):         #loop , zip can make it easier to go through and find k distance 
+        num = (abs(j-k)**2)     #calculate distance
+        total += num
+    return (total**(1/2))       
 
 def forwardSelection(data):
     dataFeatures = data.shape[1]        #get num of features
@@ -121,23 +138,23 @@ def forwardSelection(data):
         for j in range(i, dataFeatures):    #innter loop for i in dataFeatures
             if j in current_set:            #skip j in current set
                 continue
-            tempFeatures = current_set + [0] + [j]
+            tempFeatures = [0] + current_set + [j]
             tempAccuracy = leave_one_out_x_valid(data[:,tempFeatures])
-            print('Using feature(s) ',current_set+[j] ,' accuracy is ', tempAccuracy)
+            print('\tUsing feature(s) ',current_set+[j] ,' accuracy is ', round(tempAccuracy,2), '%.' )
 
-            if(max(tempAccuracy, tempMax) == tempAccuracy):
+            if(tempMax < tempAccuracy):
                 tempMax = tempAccuracy      #assign max to current max accuracy 
                 featList = j
 
         if featList:    #if not 0
             current_set.append(featList)
-            if(max(accuracyMax, tempMax) == tempMax):  #check max btw tempMax and overall accuracy 
+            if(accuracyMax < tempMax):  #check max btw tempMax and overall accuracy 
                 accuracyMax = tempMax
                 featBest[:] = current_set
-                print('Feature set {', current_set,'} was best, accuracy is ', tempMax)
+                print('Feature set ', current_set,' was best, accuracy is ', round(tempMax,2), '%.' )
             else:
-                print('Feature set {', current_set,'} was best, accuracy is ', tempMax)
-    print('Finished search! The best feature subset is ', featBest,', which has an accuracy of ', accuracyMax, '%.' )
+                print('Feature set ', current_set,' was best, accuracy is ', round(tempMax,2), '%.' )
+    print('\nFinished search! The best feature subset is ', featBest,', which has an accuracy of ', round(accuracyMax,2), '%.')
 
 
 if __name__ == "__main__":
